7af554d557628339da52f5afe12f7c95e2811b94
Yanosh Kunsh <me@yanosh.net>
2022-09-20 17:29:38 +0300

    move bulgarian_dictionary.jsonl to gitlfs

--- convertors/templates/opf.php
+++ convertors/templates/opf.php
@@ -1,35 +1,3 @@
-﻿<?xml version="1.0"?><!DOCTYPE package SYSTEM "oeb1.ent">
-<package unique-identifier="uid" xmlns:dc="Dublin Core">
-    <metadata>
-        <dc-metadata>
-            <dc:Identifier id="uid">bulgarian_dictionary</dc:Identifier>
-            <dc:Title><h2>Bulgarian Dictionary</h2></dc:Title>
-            <dc:Creator>Yanosh Kunsh</dc:Creator>
-            <dc:Language>bg</dc:Language>
-        </dc-metadata>
-        <x-metadata>
-            <DictionaryInLanguage>bg</DictionaryInLanguage>
-            <DictionaryOutLanguage>bg</DictionaryOutLanguage>
-        </x-metadata>
-    </metadata>
-
-    <manifest>
-        <item id="cover" href="cover.html" media-type="application/xhtml+xml" />
-        <?php for ($i = 0; $i < $manifestItemsCount; ++$i):?>
-            <item id="dictionary<?= $i;?>" href="bulgarian_dictionary<?= $i;?>.html" media-type="text/x-oeb1-document" />
-        <?php endfor;?>
-    </manifest>
-
-    <spine>
-        <itemref idref="cover" />
-        <?php for ($i = 0; $i < $manifestItemsCount; ++$i):?>
-            <itemref idref="dictionary<?= $i;?>" />
-        <?php endfor;?>
-    </spine>
-
-    <tours />
-
-    <guide>
-        <reference type="search" title="Dictionary Search" onclick="index_search()"/>
-    </guide>
-</package>
+version https://git-lfs.github.com/spec/v1
+oid sha256:56e82fe1a266bd6fe57f09b41ed82dc7e99ee1b2a5db105707a51f7e1e99e428
+size 1243

36941a3f5f990bc2554f5bbe37347a643376eb08
Yanosh Kunsh <me@yanosh.net>
2022-09-20 17:58:24 +0300

    revet git lfs for main files

--- convertors/templates/html.php
+++ convertors/templates/html.php
@@ -1,3 +1,30 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0fb9ab2a4eb7fcde34e5eb9d6dd985b4ba8e255aab1f2cdab995450ced8336f2
-size 789
+﻿<?xml version="1.0" encoding="utf-8"?>
+<html xmlns:idx="www.mobipocket.com" xmlns:mbp="www.mobipocket.com" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <body>
+    <mbp:frameset>
+
+<?php foreach ($words as $wordItem): ?>
+	<idx:entry name="headword" scriptable="yes">
+		<a id="<?= $wordItem['id']; ?>"></a>
+		<h2>
+			<idx:orth value="<?= $wordItem['base_word']; ?>"><?= $wordItem['base_word']; ?>
+
+				<?php if ($wordItem['derivative_forms']): ?>
+					
+					<idx:infl>
+	<?php foreach ($wordItem['derivative_forms'] as $derivativeForm): ?>
+	<idx:iform value="<?= $derivativeForm; ?>"></idx:iform>
+	<?php endforeach; ?>
+					</idx:infl>
+				<?php endif; ?>
+				
+			</idx:orth>
+		</h2>
+		<?= $wordItem['meaning']; ?>
+
+	</idx:entry>
+<?php endforeach; ?>
+
+    </mbp:frameset>
+</body>
+</html>
